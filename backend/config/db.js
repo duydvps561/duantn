7c906529ce8147d360da50a544393213eb0f54b9
NguyenPhucHau1 <hauscvippro@gmail.com>
2024-09-16 21:48:22 +0700

    Hau

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,17 +1,15 @@
 const mongoose = require('mongoose');
 mongoose.set('strictQuery', true);
 
-const atlas = 'mongodb+srv://duantotnghiep:0337221649hau@duantotnghiep.hagmok9.mongodb.net/duantotnghiep';
+const atlas = 'mongodb+srv://hau23022004:duantotnghiep1@canalisdb.l7b8nzd.mongodb.net/duantotnghiep';
 // const atlas = 'mongodb://localhost:27017/.....';
+
 const connect = async () => {
     try {
-        await mongoose.connect(atlas, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        });
+        await mongoose.connect(atlas);
         console.log('Connected successfully');
     } catch (err) {
-        console.log(err);
+        console.log('Connection error:', err);
     }
 }
 
