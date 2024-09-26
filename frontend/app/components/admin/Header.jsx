13813add7a792f0124b5057b0ee8f4299af15f12
Ngoctran1211 <ngoctrannguyenthi1211@gmail.com>
2024-09-26 10:50:14 +0700

    Tran: dashboard

---
+++ frontend/app/components/admin/Header.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header style={headerStyle}>
+      <div style={titleContainerStyle}>
+        <h1 style={leftTitleStyle}>Thống Kê</h1>
+      </div>
+    </header>
+  );
+};
+
+const headerStyle = {
+  backgroundColor: '#333',
+  color: '#fff',
+  padding: '10px',
+  textAlign: 'center',
+};
+
+const titleContainerStyle = {
+  display: 'flex',
+  justifyContent: 'space-between',
+  alignItems: 'center',
+  padding: '0 20px', // Add some horizontal padding
+};
+
+const leftTitleStyle = {
+  margin: 0, // Remove default margin
+};
+
+const rightTitleStyle = {
+  margin: 0, // Remove default margin
+};
+
+export default Header;
