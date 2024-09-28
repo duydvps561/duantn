609f93bf6ee357dc5a2888f6fd1fbb2452309902
duydvps561 <dovanduy230904@gmail.com>
2024-09-29 00:42:46 +0700

    thay ảnh

--- frontend/app/components/slide.jsx
+++ frontend/app/components/slide.jsx
@@ -4,13 +4,13 @@ export default function Slide() {
             <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
                 <div class="carousel-inner">
                     <div class="carousel-item active">
-                        <img src="/img/0017851.webp" class="d-block w-100" alt="da"></img>
+                        <img src="/img/0017851.jpg" class="d-block w-100" alt="da"></img>
                     </div>
                     <div class="carousel-item">
-                        <img src="/img/0017851.webp" class="d-block w-100" alt="da"></img>
+                        <img src="/img/slide-1.png" class="d-block w-100" alt="da"></img>
                     </div>
                     <div class="carousel-item">
-                        <img src="/img/0017851.webp" class="d-block w-100" alt="da"></img>
+                        <img src="/img/slide-2.png" class="d-block w-100" alt="da"></img>
                     </div>
                 </div>
             </div>
