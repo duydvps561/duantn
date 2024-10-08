ac03e123cbb10c739104cf6e669670ed4554cfe4
tranbebi <bit92785@gmail.com>
2024-10-08 20:49:04 +0700

    router tài khoản ok

--- backend/routes/account/taikhoan.js
+++ backend/routes/account/taikhoan.js
@@ -8,10 +8,10 @@ const path = require('path');
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, path.join(__dirname, '../../public/img')); 
+    cb(null, path.join(__dirname, '../../public/img/user')); 
   },
   filename: function (req, file, cb) {
-    cb(null, file.originalname); 
+    cb(null, file.originalname);  
   }
 });
 function checkFileUpload(req, file, cb) {
