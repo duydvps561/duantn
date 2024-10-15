3345c170195a0688ada6efcded257736e29c89bb
duydvps561 <dovanduy230904@gmail.com>
2024-10-15 07:17:37 +0700

    install axios

--- frontend/app/lichchieu/page.jsx
+++ frontend/app/lichchieu/page.jsx
@@ -11,10 +11,10 @@ export default function lichchieu() {
         </div>
         <p class="node fw-bold">Lưu ý: Khán giả dưới 13 tuổi chỉ chọn suất chiếu kết thúc trước 22h và Khán giả dưới 16 tuổi chỉ chọn suất chiếu kết thúc trước 23h.</p>
         <div class="main d-flex col-12">
-            <div class="card mb-3 bg-dark" style={{maxWidth: "540px"}}>
+            <div class="card mb-3 bg-dark" style={{maxWidth: "550px", height: "300px"}}>
                 <div class="row g-0">
-                  <div class="col-md-4">
-                    <img src="img/0017840_0 1-1.png" class="img-fluid rounded-start" style={{height: "270px"}} alt="..."/>
+                  <div class="col-md-5">
+                    <img src="img/0017840_0 1-1.png" class="img-fluid rounded-start" style={{maxWidth: "230px", height: "300px"}} alt="..."/>
                   </div>
                   <div class="col-md-7">
                     <div class="card-body text-start">
@@ -29,7 +29,6 @@ export default function lichchieu() {
                       <p class="card-text text-light">Lịch chiếu</p>
                       <div class="giochieu">
                         <ul class="time">
-                            <li><a class="timechieu text-light border border-light-subtle">11h30</a></li>
                             <li><a class="timechieu text-light border border-light-subtle">11h30</a></li>
                             <li><a class="timechieu text-light border border-light-subtle">11h30</a></li>
                             <li><a class="timechieu text-light border border-light-subtle">11h30</a></li>
@@ -40,10 +39,10 @@ export default function lichchieu() {
                   </div>
                 </div>
             </div>
-            <div class="card mb-3 bg-dark" style={{maxWidth: "540px"}}>
+            <div class="card mb-3 bg-dark" style={{maxWidth: "550px", height: "300px"}}>
                 <div class="row g-0">
-                  <div class="col-md-4">
-                    <img src="img/0017840_0 1-1.png" class="img-fluid rounded-start" style={{height: "270px"}} alt="..."/>
+                  <div class="col-md-5">
+                    <img src="img/0017840_0 1-1.png" class="img-fluid rounded-start" style={{maxWidth: "230px", height: "300px"}} alt="..."/>
                   </div>
                   <div class="col-md-7">
                     <div class="card-body text-start">
@@ -58,7 +57,6 @@ export default function lichchieu() {
                       <p class="card-text text-light">Lịch chiếu</p>
                       <div class="giochieu">
                         <ul class="time">
-                            <li><a class="timechieu text-light border border-light-subtle">11h30</a></li>
                             <li><a class="timechieu text-light border border-light-subtle">11h30</a></li>
                             <li><a class="timechieu text-light border border-light-subtle">11h30</a></li>
                             <li><a class="timechieu text-light border border-light-subtle">11h30</a></li>
@@ -71,64 +69,62 @@ export default function lichchieu() {
             </div>
         </div>
         <div class="main d-flex col-12">
-            <div class="card mb-3 bg-dark" style={{maxWidth: "540px"}}>
-                <div class="row g-0">
-                  <div class="col-md-4">
-                    <img src="img/0017840_0 1-1.png" class="img-fluid rounded-start" style={{height: "270px"}} alt="..."/>
-                  </div>
-                  <div class="col-md-7">
-                    <div class="card-body text-start">
-                        <div class="timeline d-flex  text-light">
-                            <p class="infor">Hai</p>
-                            <p class="infor-time">110 phut</p>
-                        </div>
-                      <h5 class="card-title text-light">ANH TRAI VƯỢT MỌI TAM TAI</h5>
-                      <p>Xuất xứ: Mỹ</p>
-                      <p>Khởi chiếu: 14/09/2024</p>
-                      <p class="card-text-node">T18 - Phim được phổ biến đến người xem từ đủ 18 tuổi trở lên (18+)</p>
-                      <p class="card-text text-light">Lịch chiếu</p>
-                      <div class="giochieu">
-                        <ul class="time">
-                            <li><a class="timechieu text-light border border-light-subtle">11h30</a></li>
-                            <li><a class="timechieu text-light border border-light-subtle">11h30</a></li>
-                            <li><a class="timechieu text-light border border-light-subtle">11h30</a></li>
-                            <li><a class="timechieu text-light border border-light-subtle">11h30</a></li>
-                            <li><a class="timechieu text-light border border-light-subtle">11h30</a></li>
-                        </ul>
-                      </div>
-                    </div>
-                  </div>
+        <div class="card mb-3 bg-dark" style={{maxWidth: "550px", height: "300px"}}>
+        <div class="row g-0">
+          <div class="col-md-5">
+            <img src="img/0017840_0 1-1.png" class="img-fluid rounded-start" style={{maxWidth: "230px", height: "300px"}} alt="..."/>
+          </div>
+          <div class="col-md-7">
+            <div class="card-body text-start">
+                <div class="timeline d-flex  text-light">
+                    <p class="infor">Hai</p>
+                    <p class="infor-time">110 phut</p>
                 </div>
+              <h5 class="card-title text-light">ANH TRAI VƯỢT MỌI TAM TAI</h5>
+              <p>Xuất xứ: Mỹ</p>
+              <p>Khởi chiếu: 14/09/2024</p>
+              <p class="card-text-node">T18 - Phim được phổ biến đến người xem từ đủ 18 tuổi trở lên (18+)</p>
+              <p class="card-text text-light">Lịch chiếu</p>
+              <div class="giochieu">
+                <ul class="time">
+                    <li><a class="timechieu text-light border border-light-subtle">11h30</a></li>
+                    <li><a class="timechieu text-light border border-light-subtle">11h30</a></li>
+                    <li><a class="timechieu text-light border border-light-subtle">11h30</a></li>
+                    <li><a class="timechieu text-light border border-light-subtle">11h30</a></li>
+                </ul>
+              </div>
             </div>
-            <div class="card mb-3 bg-dark" style={{maxWidth: "540px"}}>
-                <div class="row g-0">
-                  <div class="col-md-4">
-                    <img src="img/0017840_0 1-1.png" class="img-fluid rounded-start" style={{height: "270px"}} alt="..."/>
-                  </div>
-                  <div class="col-md-7">
-                    <div class="card-body text-start">
-                        <div class="timeline d-flex  text-light">
-                            <p class="infor">Hai</p>
-                            <p class="infor-time">110 phut</p>
-                        </div>
-                      <h5 class="card-title text-light">ANH TRAI VƯỢT MỌI TAM TAI</h5>
-                      <p>Xuất xứ: Mỹ</p>
-                      <p>Khởi chiếu: 14/09/2024</p>
-                      <p class="card-text-node">T18 - Phim được phổ biến đến người xem từ đủ 18 tuổi trở lên (18+)</p>
-                      <p class="card-text text-light">Lịch chiếu</p>
-                      <div class="giochieu">
-                        <ul class="time">
-                            <li><a class="timechieu text-light border border-light-subtle">11h30</a></li>
-                            <li><a class="timechieu text-light border border-light-subtle">11h30</a></li>
-                            <li><a class="timechieu text-light border border-light-subtle">11h30</a></li>
-                            <li><a class="timechieu text-light border border-light-subtle">11h30</a></li>
-                            <li><a class="timechieu text-light border border-light-subtle">11h30</a></li>
-                        </ul>
-                      </div>
-                    </div>
-                  </div>
-                </div>
+          </div>
+        </div>
+    </div>
+    <div class="card mb-3 bg-dark" style={{maxWidth: "550px", height: "300px"}}>
+    <div class="row g-0">
+      <div class="col-md-5">
+        <img src="img/0017840_0 1-1.png" class="img-fluid rounded-start" style={{maxWidth: "230px", height: "300px"}} alt="..."/>
+      </div>
+      <div class="col-md-7">
+        <div class="card-body text-start">
+            <div class="timeline d-flex  text-light">
+                <p class="infor">Hai</p>
+                <p class="infor-time">110 phut</p>
             </div>
+          <h5 class="card-title text-light">ANH TRAI VƯỢT MỌI TAM TAI</h5>
+          <p>Xuất xứ: Mỹ</p>
+          <p>Khởi chiếu: 14/09/2024</p>
+          <p class="card-text-node">T18 - Phim được phổ biến đến người xem từ đủ 18 tuổi trở lên (18+)</p>
+          <p class="card-text text-light">Lịch chiếu</p>
+          <div class="giochieu">
+            <ul class="time">
+                <li><a class="timechieu text-light border border-light-subtle">11h30</a></li>
+                <li><a class="timechieu text-light border border-light-subtle">11h30</a></li>
+                <li><a class="timechieu text-light border border-light-subtle">11h30</a></li>
+                <li><a class="timechieu text-light border border-light-subtle">11h30</a></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+</div>
         </div>
     </section>
     )
