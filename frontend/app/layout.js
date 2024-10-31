118ac6dec7da55d6d668406f18edb15eb698581c
Minh Tuong <tuongphan2004@gmail.com>
2024-10-31 23:50:29 +0700

    them tongtien hoa don, block thanh toan khi cart=rong,trang thanh toan thanh cong

--- frontend/app/layout.js
+++ frontend/app/layout.js
@@ -5,15 +5,18 @@ import '../public/bootstrap-5.3.3-dist/js/bootstrap.bundle.min.js';
 import Header from "./components/header";
 import Footer from "./components/footer";
 import Providers from "@/redux/Provider";
+import { usePathname } from "next/navigation";
 
 export default function RootLayout({ children }) {
+  const pathname = usePathname();
+    const isThanhCong = pathname.includes('thanhtoan/thanhcong');
   return (
     <html lang="en">
       <body style={{ backgroundColor: '#050B17' }}>
         <Providers>
         <Header />
         {children}
-        <Footer/>
+        {!isThanhCong && <Footer />}
         </Providers>
         <script src="https://kit.fontawesome.com/ea6209cd9f.js" crossorigin="anonymous"></script>
         </body>
--- frontend/app/thanhtoan/thanhcong/page.jsx
+++ frontend/app/thanhtoan/thanhcong/page.jsx
@@ -1,10 +1,26 @@
+import '../thanhtoan.css'
 export default function ThanhCong() {
+    
     return (
-        <>
-        <div className="container bg-success">
-            <h1>Thông báo</h1>
-            <p>Thanh toans thanh cong!</p>
+        <div className="container d-flex justify-content-center flex-column align-items-center">
+            <div className="check-icon-box">
+                <div className="circle-icon">
+                    <svg width="53" height="53" viewBox="0 0 64 64" fill="#4CAF50" xmlns="http://www.w3.org/2000/svg">
+                        <circle cx="32" cy="32" r="30" stroke="#25282E" strokeWidth="0" />
+                        <path d="M22 32L28 38L42 24" stroke="#25282E" strokeWidth="5" strokeLinecap="round" strokeLinejoin="round" />
+                    </svg>
+                </div>
+            </div>
+            <span className='fs-2 fw-bold text-light mt-1'>Thanh toán thành công!</span>
+            <span className='text-light mt-1 fs-5 opacity-1 mb-3'>Cảm ơn bạn đã mua vé tại ACE Cinema</span>
+            <div className='navi-btn d-flex gap-3'>
+                <button className='btn btn-secondary'>
+                    Lịch sử mua vé
+                </button>
+                <button className='btn btn-secondary'>
+                    Xem vé vừa mua
+                </button>
+            </div>
         </div>
-        </>
-    )
+    );
 }
