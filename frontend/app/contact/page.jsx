208526e77fb2dda35bdd9c3fcb4c8e3fec13256d
duydvps561 <dovanduy230904@gmail.com>
2024-11-05 13:58:12 +0700

    fix contact

--- frontend/app/contact/page.jsx
+++ frontend/app/contact/page.jsx
@@ -1,64 +1,92 @@
-
-import React from 'react';
-import './contact.css'
+import React from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLocation } from '@fortawesome/free-solid-svg-icons'; // Nhập biểu tượng vị trí
+import "./contact.css";
 
 const ContactPage = () => {
   return (
-    <div className="container justify-content-center">
-      <h2>Liên Hệ</h2>
-      <div className="contact d-flex">
-        <div className="contact-infor">
-          <div className="infor">
-            <h4>Trụ Sở</h4>
-            <p>Số 87 Láng Hạ, Phường Thành Công, Quận Ba Đình, Tp. Hà Nội</p>
+  <div className="contact-container">
+  <div className="contact-row">
+    <h1>Liên hệ</h1>
+    <div className="contact-top-content">
+      <div className="contact-form-col-8">
+        <form className="contact-form" action="">
+          <div className="contact-information">
+            <input type="email" className="contact-input" placeholder="Email" required />
+            <input type="tel" className="contact-input" placeholder="Số điện thoại" required />
           </div>
-          <div className="infor">
-            <h4>Hỗ Trợ Khách Hàng</h4>
-            <p>Hotline: 012346789</p>
-            <p>Giờ làm việc: 8:00 - 22:00</p>
-            <p>Tất cả các ngày bao gồm ngày lễ</p>
-            <p>Email hỗ trợ: ABC123@gmail.com</p>
-          </div>
-          <div className="infor">
-            <h4>Liên Hệ Quảng Cáo, Tổ Chức Sự Kiện, Thuê Rạp</h4>
-            <p>Phòng Dịch Vụ</p>
-            <p>Hotline: 012346789</p>
-            <p>Email hỗ trợ: ABC123@gmail.com</p>
-          </div>
-          <div className="infor">
-            <h4>Liên Hệ Mua Vé Hợp Đồng</h4>
-            <p>Phòng Chiếu Phim Và Trưng Bày Sản Phẩm Điện Ảnh</p>
-            <p>Hotline: 012346789</p>
-            <p>Email hỗ trợ: ABC123@gmail.com</p>
-          </div>
-        </div>
-        <div className="map">
-          <iframe
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3724.395664587564!2d105.81305067548594!3d21.016848788187108!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3135ab9ef82fed13%3A0x1ee04aa292e377a2!2zVHJ1bmcgdMOibSBDaGnhur91IHBoaW0gUXXhu5FjIGdpYQ!5e0!3m2!1svi!2s!4v1727341215756!5m2!1svi!2s"
-            width="600"
-            height="450"
-            style={{ border: 0 }}
-            allowFullScreen
-            loading="lazy"
-            referrerPolicy="no-referrer-when-downgrade"
-          />
-        </div>
-      </div>
-      <div className="form-contact">
-        <form action="">
-          <label htmlFor="fname">Họ và Tên</label><br/>
-          <input type="text" id="fname" name="fname" placeholder="Họ và Tên" /><br/>
-          <label htmlFor="lname">Số điện thoại</label><br/>
-          <input type="text" id="lname" name="lname" placeholder="Số điện thoại" /><br/>
-          <label htmlFor="email">Email</label><br/>
-          <input type="text" id="email" name="email" placeholder="Email" /><br/>
-          <label htmlFor="message">Yêu cầu từ khách hàng</label><br/>
-          <input type="text" id="message" name="message" placeholder="Nội dung..." /><br/>
-          <input type="submit" value="Gửi Thông Tin" />
+          <input type="text" className="contact-input" placeholder="Họ và Tên" required />
+          <textarea className="contact-textarea" placeholder="Messenger...."></textarea>
+          <button className="contact-send-btn" type="submit">Send Messenger</button>
         </form>
       </div>
+      <div className="contact-info-col-4">
+        <div className="contact-info">
+          <h1 className="contact-title">Tư Vấn Khách Hàng</h1>
+          <p className="contact-description">
+            Mọi sự thắc mắc của quý khách sẽ được chúng tôi giải đáp khi quý
+            khách yêu cầu.
+            <br />
+            Để giải đáp thắc mắc vui lòng gửi thông tin email cho chúng tôi
+          </p>
+          <div className="contact-send-email">
+            <input type="email" className="contact-input" placeholder="Email" required />
+            <button className="contact-email-btn">Send Email</button>
+          </div>
+        </div>
+      </div>
     </div>
-  );
+    <div className="contact-middle-content">
+      <div id="contact-box-phone" className="contact-box col-4">
+        <div className="contact-card">
+          <div className="contact-title">
+            <i className="fa fa-phone" style={{ fontSize: '36px' }}>
+              0123456789
+            </i>
+          </div>
+          <p className="contact-info-description">Gọi vào số hotline này để được tư vấn</p>
+        </div>
+      </div>
+      <div id="contact-box-mail" className="contact-box col-4">
+        <div className="contact-card">
+          <div className="contact-title">
+            <i className="fa fa-envelope" style={{ fontSize: '36px' }}>
+              abcxyz@gmail.com
+            </i>
+          </div>
+          <p className="contact-info-description">Gọi vào số hotline này để được tư vấn</p>
+        </div>
+      </div>
+      <div id="contact-box-location" className="contact-box col-4">
+        <div className="contact-card">
+          <div className="contact-title">
+          <i className="fa fa-map-marker" style={{ fontSize: '36px' }}>
+            Địa chỉ nè baaaaaaa
+          </i>
+          </div>
+          <p className="contact-info-description">Gọi vào số hotline này để được tư vấn</p>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div className="contact-bot-content">
+    <div className="contact-map-col-12">
+      <div className="contact-map">
+        <iframe
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d12498.679263964908!2d106.68228527684877!3d10.761607947164666!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31752f19e63f0a05%3A0xd4c41b784ebcc3e8!2sCinestar%20Cinema%20Qu%E1%BB%91c%20Thanh!5e0!3m2!1svi!2s!4v1730452044126!5m2!1svi!2s"
+          width="100%"
+          height="600"
+          style={{ border: 0 }}
+          allowFullScreen
+          loading="lazy"
+          referrerPolicy="no-referrer-when-downgrade"
+        ></iframe>
+      </div>
+    </div>
+  </div>
+</div>
+);
 };
 
 export default ContactPage;
--- frontend/app/middleware.js
+++ frontend/app/middleware.js
@@ -1,56 +1,27 @@
-// app/middleware.js
 import { NextResponse } from "next/server";
 
 export async function middleware(request) {
   const token = request.cookies.get("token");
 
-  // Kiểm tra xem người dùng có token hợp lệ hay không
   if (!token) {
-    return NextResponse.redirect(new URL("/login", request.url)); // Chuyển hướng đến trang đăng nhập
+    return NextResponse.redirect(new URL("/dangnhap", request.url));
   }
 
-  try {
-    // Gọi API để xác thực token
-    const res = await fetch("http://localhost:3000/login/checktoken", {
-      headers: {
-        Authorization: `Bearer ${token.value}`,
-      },
-    });
+  // Gọi đến API xác thực token
+  const res = await fetch("http://localhost:3000/checktoken", {
+    headers: {
+      Authorization: `Bearer ${token.value}`,
+    },
+  });
 
-    if (!res.ok) {
-      return NextResponse.redirect(new URL("/login", request.url)); // Chuyển hướng nếu token không hợp lệ
-    }
-
-    const userData = await res.json(); // Lấy thông tin người dùng
-
-    // Gọi API để tìm tài khoản theo email
-    const accountRes = await fetch(
-      `http://localhost:3000/taikhoan?email=${userData.email}`,
-      {
-        headers: {
-          Authorization: `Bearer ${token.value}`,
-        },
-      }
-    );
-    console.log("accountRes:", accountRes);
-
-    if (!accountRes.ok) {
-      return NextResponse.redirect(new URL("/login", request.url)); // Chuyển hướng nếu không tìm thấy tài khoản
-    }
-
-    const accountData = await accountRes.json(); // Lấy thông tin tài khoản
-
-    // Thêm thông tin tài khoản vào yêu cầu
-    request.user = { ...userData, account: accountData };
-
-    // Nếu token hợp lệ và tài khoản tồn tại, cho phép yêu cầu tiếp tục
-    return NextResponse.next();
-  } catch (error) {
-    console.error("Error in middleware:", error);
-    return NextResponse.redirect(new URL("/login", request.url)); // Chuyển hướng nếu có lỗi
+  if (!res.ok) {
+    return NextResponse.redirect(new URL("/dangnhap", request.url));
   }
+
+  // Nếu token hợp lệ, cho phép yêu cầu tiếp tục
+  return NextResponse.next();
 }
 
 export const config = {
-  matcher: ["/tintuc"], // Áp dụng middleware cho các route cần bảo vệ
+  matcher: "/info",
 };
