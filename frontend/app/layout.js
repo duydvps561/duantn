da91bc81783bba59ee4fc88245b55861589c1379
duydvps561 <dovanduy230904@gmail.com>
2024-11-05 14:22:52 +0700

    xungdot

--- frontend/app/layout.js
+++ frontend/app/layout.js
@@ -9,30 +9,20 @@ import { usePathname } from "next/navigation";
 
 export default function RootLayout({ children }) {
   const pathname = usePathname();
-    const isThanhCong = pathname.includes('thanhtoan/thanhcong');
+  const isThanhCong = pathname.includes("thanhtoan/thanhcong");
   return (
     <html lang="en">
       <body style={{ backgroundColor: "#050B17" }}>
         <Providers>
-<<<<<<< HEAD
           <Header />
           {children}
-          <Footer />
+          {!isThanhCong && <Footer />}
         </Providers>
         <script
           src="https://kit.fontawesome.com/ea6209cd9f.js"
           crossorigin="anonymous"
         ></script>
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
       </body>
-=======
-        <Header />
-        {children}
-        {!isThanhCong && <Footer />}
-        </Providers>
-        <script src="https://kit.fontawesome.com/ea6209cd9f.js" crossorigin="anonymous"></script>
-        </body>
->>>>>>> 2d358396dab62089025615c40ee5a12cd5e00907
     </html>
   );
 }
