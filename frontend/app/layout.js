3dde1cd30f02a2aa8aa98fff62156d141cd998e8
tranbebi <bit92785@gmail.com>
2024-11-07 19:58:44 +0700

    lưu

--- frontend/app/layout.js
+++ frontend/app/layout.js
@@ -1,12 +1,11 @@
 "use client";
 import "./globals.css";
 import "../public/bootstrap-5.3.3-dist/css/bootstrap.min.css";
-import "../public/bootstrap-5.3.3-dist/js/bootstrap.bundle.min.js";
+// import "../public/bootstrap-5.3.3-dist/js/bootstrap.bundle.min.js";
 import Header from "./components/header";
 import Footer from "./components/footer";
 import Providers from "@/redux/Provider";
 import { usePathname } from "next/navigation";
-
 export default function RootLayout({ children }) {
   const pathname = usePathname();
   const isThanhCong = pathname.includes("thanhtoan/thanhcong");
