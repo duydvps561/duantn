ab18be9a4c9686e389ce7ec76b1bdd4f440c6781
Minh Tuong <tuongphan2004@gmail.com>
2024-11-17 21:23:31 +0700

    tao ve xem phim

--- frontend/app/page.jsx
+++ frontend/app/page.jsx
@@ -31,31 +31,66 @@ export default function Home() {
   const minute = String(now.getMinutes()).padStart(2, '0');
   const giolap = `${hours}:${minute}`;
   const ngaylap = now.toISOString()
+  const cachieuID = '64b5f93e49e7b6e31c5f7190';
+  const hoadonID = '64b5f93e49e7b6e31c5f7191';
+  const gheID = '672b7e6ae6ff062499489b6e';
   useEffect(() => {
     const query = new URLSearchParams(window.location.search);
     // const { orderCode } = query.get('orderCode');
     const isHoadonProcessed = localStorage.getItem("hoadonProcessed");
-    if (query.get("success") && !isProcessing && !isHoadonProcessed) {
+
+    if (query.get("success") === "true" && !isProcessing && !isHoadonProcessed) {
+      setTypeNoti("success");
+      setMessage("Đang xử lý thanh toán...");
+      setShowNotification(true);
+      if (!totalAmount || !giolap || !ngaylap || !userId || !cachieuID || !gheID) {
+        console.error("Thiếu dữ liệu cần thiết cho hóa đơn hoặc vé!");
+        return;
+      }
       const hoadondata = {
         tongtien: totalAmount,
         giolap: giolap,
         ngaylap: ngaylap,
         taikhoan_id: userId,
       };
+      const ticketdata = {
+        cachieu_id: "64b5f93e49e7b6e31c5f7191",
+        hoadon_id: "6737a0d3e78ce0a5476d286b",
+        ghe_id: '672b7e6ae6ff062499489b6e',
+        giave: totalAmount
+      }
       setIsProcessing(true);
       localStorage.setItem("hoadonProcessed", "true");
-      dispatch(postHoadon(hoadondata))
-        .unwrap()
-        .then((response) => {
-          dispatch(clearCart());
-          console.log("Thanh toán thành công:", response);
-          setTypeNoti("success");
-          setMessage("Thanh toán thành công. Cảm ơn bạn đã sử dụng payOS!");
-          setShowNotification(true);
-          setTimeout(() => {
-            localStorage.setItem('hoadonProcessed', 'false');
-            router.replace("/");
-          }, 2000);
+
+      Promise.allSettled([
+        dispatch(postHoadon(hoadondata)).unwrap(),
+        dispatch(postTicket(ticketdata)).unwrap(),
+      ])
+        .then((results) => {
+          const [hoadonResult, ticketResult] = results;
+          if (hoadonResult.status === 'fulfilled' && ticketResult.status === 'fulfilled') {
+            dispatch(clearCart());
+            setTypeNoti("success");
+            setMessage("Thanh toán thành công. Cảm ơn bạn đã mua vé tại ACE Cinema");
+            setShowNotification(true);
+            setTimeout(() => {
+              localStorage.removeItem("hoadonProcessed");
+              router.replace("/");
+            }, 2000);
+          } else {
+            if (hoadonResult.status === 'rejected') {
+              console.error("Lỗi khi tạo hóa đơn:", hoadonResult.reason);
+              setTypeNoti("error");
+              setMessage("Lỗi khi tạo hóa đơn");
+              setShowNotification(true);
+            }
+            if (ticketResult.status === "rejected") {
+              console.error("Lỗi khi tạo vé:", ticketResult.reason);
+              setTypeNoti("error");
+              setMessage("Lỗi khi tạo vé. Vui lòng thử lại!");
+              setShowNotification(true);
+            }
+          }
         })
         .catch((error) => {
           console.error("Error posting hoadon:", error);
@@ -78,8 +113,8 @@ export default function Home() {
       }, 2000);
     }
 
-  }, [router, totalAmount, giolap, ngaylap, dispatch, isProcessing]); 
-  
+  }, [router, totalAmount, giolap, ngaylap, dispatch, isProcessing]);
+
   useEffect(() => {
     const fetchMovies = async () => {
       try {
@@ -111,8 +146,8 @@ export default function Home() {
           </div>
           <div className="card-body">
             <div className="day-time">
-              <p style={{color:'#fff'}}>{movie.thoiluong} Phút</p>
-              <p style={{color:'#fff'}}>{new Date(movie.ngayhieuluc).toLocaleDateString("vi-VN")}</p>
+              <p style={{ color: '#fff' }}>{movie.thoiluong} Phút</p>
+              <p style={{ color: '#fff' }}>{new Date(movie.ngayhieuluc).toLocaleDateString("vi-VN")}</p>
             </div>
             <div className="title-card">
               <h1>{movie.tenphim}</h1>
