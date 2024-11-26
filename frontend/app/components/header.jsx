c3c1f77e39126bb6f343dcde48c217e4c88337f4
duydvps561 <dovanduy230904@gmail.com>
2024-11-26 23:30:26 +0700

    luu

--- frontend/app/components/header.jsx
+++ frontend/app/components/header.jsx
@@ -3,7 +3,6 @@ import Link from "next/link";
 import { usePathname } from "next/navigation";
 import LoginModal from "../login/LoginModal";
 import RegisterModal from "../register/RegisterModal";
-import UserInfo from "../userinfor/page.jsx";
 import { useDispatch, useSelector } from "react-redux";
 import { logout } from "@/redux/slice/authSlice";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -24,22 +23,25 @@ export default function Header() {
   const pathname = usePathname();
   const [showRegister, setRegister] = useState(false);
   const [showLogin, setShowLogin] = useState(false);
-  const [showUserInfo, setShowUserInfo] = useState(false);
-  const [showOp, setShowOp] = useState(false);
 
   const handleOpenRegister = () => setRegister(true);
   const handleCloseRegister = () => setRegister(false);
   const handleOpenLogin = () => setShowLogin(true);
   const handleCloseLogin = () => setShowLogin(false);
 
-  const toggleUserInfo = () => {
-    setShowUserInfo((prev) => !prev);
-  };
+  // Log userId to check
+  useEffect(() => {
+    if (user) {
+      console.log("User ID:", user.id);
+    } else {
+      console.log("User is not authenticated");
+    }
+  }, [user]);
 
   return (
     <header>
       <Link href="/">
-        <img src="/img/logo.png" alt="anh logo" />
+        <img src="/img/logo.png" alt="Logo" />
       </Link>
       <nav>
         <ul className="nav-list">
@@ -56,84 +58,75 @@ export default function Header() {
         </ul>
       </nav>
       {isAuthen && user ? (
-        <>
-          {
-            <div className="dropdown">
-              <div
-                className="d-flex align-items-center gap-3"
-                id="dropdownMenuButton"
-                data-bs-toggle="dropdown"
-                aria-expanded="false"
-                style={{ textDecoration: "none", cursor: "pointer" }}
-              >
-                <div
-                  style={{
-                    backgroundColor: "#202d32",
-                    width: "30px",
-                    height: "30px",
-                    borderRadius: "50%",
-                  }}
-                  className="d-flex align-items-center justify-content-center"
-                >
-                  <FontAwesomeIcon
-                    icon={faUser}
-                    style={{ color: "#ffffff", fontSize: "14px" }}
-                  />
-                </div>
-                <span
-                  className="text-light"
-                  style={{ fontSize: "17px" }}
-                  onClick={() => setShowOp(true)}
-                >
-                  {user.username}
-                </span>
-                <FontAwesomeIcon
-                  icon={faChevronDown}
-                  style={{ color: "#ffffff", fontSize: "17px" }}
-                />
-              </div>
-              <ul
-                className="dropdown-menu mt-2"
-                style={{ backgroundColor: "#10141b" }}
-                aria-labelledby="dropdownMenuButton"
-              >
-                <li>
-                  <a
-                    className="dropdown-item "
-                    href="#"
-                    style={{ fontSize: "17px" }}
-                    onClick={toggleUserInfo}
-                  >
-                    Thông tin cá nhân
-                  </a>
-                </li>
-                <li>
-                  <a
-                    className="dropdown-item "
-                    href="/yourticket"
-                    style={{ fontSize: "17px" }}
-                  >
-                    <i className="fa-solid fa-arrow-right-from-bracket"></i>{" "}
-                    Lịch sử mua vé
-                  </a>
-                </li>
-                <li>
-                  <a
-                    className="dropdown-item "
-                    href="#"
-                    style={{ fontSize: "17px" }}
-                    onClick={() => {
-                      setShowOp(false), dispatch(logout());
-                    }}
-                  >
-                    <i className="fa-solid fa-arrow-right-from-bracket"></i>{" "}
-                    Đăng xuất
-                  </a>
-                </li>
-              </ul>
+        <div className="dropdown">
+          <div
+            className="d-flex align-items-center gap-3"
+            id="dropdownMenuButton"
+            data-bs-toggle="dropdown"
+            aria-expanded="false"
+            style={{ textDecoration: "none", cursor: "pointer" }}
+          >
+            <div
+              style={{
+                backgroundColor: "#202d32",
+                width: "30px",
+                height: "30px",
+                borderRadius: "50%",
+              }}
+              className="d-flex align-items-center justify-content-center"
+            >
+              <FontAwesomeIcon
+                icon={faUser}
+                style={{ color: "#ffffff", fontSize: "14px" }}
+              />
             </div>
-          }
-        </>
+            <span className="text-light" style={{ fontSize: "17px" }}>
+              {user.username}
+            </span>
+            <FontAwesomeIcon
+              icon={faChevronDown}
+              style={{ color: "#ffffff", fontSize: "17px" }}
+            />
+          </div>
+          <ul
+            className="dropdown-menu mt-2"
+            style={{ backgroundColor: "#10141b" }}
+            aria-labelledby="dropdownMenuButton"
+          >
+            <li>
+              <Link
+                href={`/user`}
+                className="dropdown-item"
+                style={{ fontSize: "17px" }}
+              >
+                Thông tin cá nhân
+              </Link>
+            </li>
+            <li>
+              <a
+                className="dropdown-item"
+                href="#"
+                style={{ fontSize: "17px" }}
+              >
+                <i className="fa-solid fa-arrow-right-from-bracket"></i> Lịch sử
+                mua vé
+              </a>
+            </li>
+            <li>
+              <a
+                className="dropdown-item"
+                href="#"
+                style={{ fontSize: "17px" }}
+                onClick={() => {
+                  dispatch(logout());
+                }}
+              >
+                <i className="fa-solid fa-arrow-right-from-bracket"></i> Đăng
+                xuất
+              </a>
+            </li>
+          </ul>
+        </div>
       ) : (
         <div className="d-flex gap-3">
           <button className="regist-btn" onClick={handleOpenRegister}>
@@ -144,11 +137,9 @@ export default function Header() {
           </button>
         </div>
       )}
+
       <RegisterModal show={showRegister} handleClose={handleCloseRegister} />
       <LoginModal show={showLogin} handleClose={handleCloseLogin} />
-      {showUserInfo && user && (
-        <UserInfo userId={user.id} onClose={toggleUserInfo} />
-      )}{" "}
     </header>
   );
 }
