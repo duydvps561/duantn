9fb6dd5ccf9c13b559bc73fcd24f51fbfe924964
duydvps561 <dovanduy230904@gmail.com>
2024-11-28 20:30:02 +0700

    xungdot

--- backend/routes/food/hoadon.js
+++ backend/routes/food/hoadon.js
@@ -23,10 +23,9 @@ router.post("/add", async (req, res) => {
     res.status(500).send({ error: err.message });
   }
 });
-<<<<<<< HEAD
 
 // Get invoice by ID
-router.get('/:id', async (req, res) => {
+router.get("/:id", async (req, res) => {
   try {
     const hoadon = await Hoadon.findById(req.params.id).populate(
       "taikhoan_id",
